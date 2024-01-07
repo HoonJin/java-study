6b8be818940c363856db79d93022549a620c7b41
Daniel Ji <bwjhj1030@gmail.com>
2024-01-07 19:47:05 +0900

    멀티모듈화 및 자바와 스프링을 별도 디렉터리로  모듈 분리

---
+++ spring/rest1/src/main/java/com/hoonjin/study/spring/rest1/Rest1Application.java
@@ -0,0 +1,18 @@
+package com.hoonjin.study.spring.rest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rest1Application {
+
+    public static void main(String[] args) {
+//        ApplicationContext ac = SpringApplication.run(Rest1Application.class, args);
+//
+//        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+//        for (String bean : beanDefinitionNames) {
+//            System.out.println("bean = " + bean);
+//        }
+        SpringApplication.run(Rest1Application.class, args);
+    }
+}
