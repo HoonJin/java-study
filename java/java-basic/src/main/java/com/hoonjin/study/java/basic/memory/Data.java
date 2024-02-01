136efceee071b9b89d21f6faa00e947c7056dbd3
Daniel Ji <bwjhj1030@gmail.com>
2024-02-02 00:46:22 +0900

    자바 스택 힙 메모리에 대한 예제 추가

---
+++ java/java-basic/src/main/java/com/hoonjin/study/java/basic/memory/Data.java
@@ -0,0 +1,13 @@
+package com.hoonjin.study.java.basic.memory;
+
+import lombok.Getter;
+
+@Getter
+public class Data {
+
+    private int value;
+
+    public Data(int value) {
+        this.value = value;
+    }
+}
