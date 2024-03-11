298893c244b79a8f80226648ccc64116eae374ee
Daniel Ji <bwjhj1030@gmail.com>
2024-03-11 19:44:47 +0900

    스프링 입문 강의 시작 빌드 및 기본 컨트롤러, 정적컨텐츠 구현

---
+++ spring/spring-start/src/main/java/com/hoonjin/study/spring/start/SpringStartApplication.java
@@ -0,0 +1,12 @@
+package com.hoonjin.study.spring.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStartApplication.class, args);
+    }
+}
