b09e43b2159ae55d267d22077d43356d45785615
Daniel Ji <bwjhj1030@gmail.com>
2024-07-23 17:07:00 +0900

    서블릿 등록 및 요청 처리

--- spring/spring-hello/src/main/java/com/hoonjin/study/spring/hello/SpringHelloApplication.java
+++ spring/spring-hello/src/main/java/com/hoonjin/study/spring/hello/SpringHelloApplication.java
@@ -1,12 +1,38 @@
 package com.hoonjin.study.spring.hello;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootApplication
+import java.io.IOException;
+
+//@SpringBootApplication
 public class SpringHelloApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringHelloApplication.class, args);
+//        SpringApplication.run(SpringHelloApplication.class, args);
+
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(
+            servletContext -> {
+                servletContext.addServlet("hello", new HttpServlet() {
+                    @Override
+                    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+                        String name = req.getParameter("name");
+
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().write("Hello " + name);
+                    }
+                }).addMapping("/hello");
+            }
+        );
+        webServer.start();
     }
 }
