a40640259fad6c01ef224f64375b0da45e93399a
Daniel Ji <bwjhj1030@gmail.com>
2024-07-26 13:26:41 +0900

    front controller 를 dispatcherServelt 으로 대체, 어노테이션 기반으로 빈을 등록하는 방식 도입

--- spring/spring-hello/src/main/java/com/hoonjin/study/spring/hello/SpringHelloApplication.java
+++ spring/spring-hello/src/main/java/com/hoonjin/study/spring/hello/SpringHelloApplication.java
@@ -1,48 +1,43 @@
 package com.hoonjin.study.spring.hello;
 
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-import java.io.IOException;
-
-//@SpringBootApplication
+@Configuration
 public class SpringHelloApplication {
+
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
 
     public static void main(String[] args) {
 
-        GenericApplicationContext ac = new GenericApplicationContext();
-        ac.registerBean(HelloController.class);
-        ac.registerBean(SimpleHelloService.class);
+        AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext ->
+                    servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this))
+                        .addMapping("/*")
+                );
+                webServer.start();
+            }
+        };
+
+        ac.register(SpringHelloApplication.class);
         ac.refresh();
-
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext ->
-                servletContext.addServlet("frontcontroller", new HttpServlet() {
-                    @Override
-                    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-                        // 인증, 보안, 다국어 등 공통 기능
-
-                        if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                            String name = req.getParameter("name");
-                            HelloController helloController = ac.getBean(HelloController.class);
-                            String body = helloController.hello(name);
-
-                            resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                            resp.getWriter().write(body);
-                        } else {
-                            resp.setStatus(HttpStatus.NOT_FOUND.value());
-                        }
-                    }
-                }).addMapping("/*")
-        );
-        webServer.start();
     }
 }
