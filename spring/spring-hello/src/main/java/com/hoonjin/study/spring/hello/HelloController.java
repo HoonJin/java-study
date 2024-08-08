6b02dc563fa6a63136ee47591e6bdc1dacccbad9
Daniel Ji <bwjhj1030@gmail.com>
2024-08-08 15:39:23 +0900

    dispatcherServlet 생성시 web application context를 주입하지 않더라도 빈으로 만들면 자동으로 주입됨을 확인, main 메서드 정리

--- spring/spring-hello/src/main/java/com/hoonjin/study/spring/hello/HelloController.java
+++ spring/spring-hello/src/main/java/com/hoonjin/study/spring/hello/HelloController.java
@@ -1,14 +1,18 @@
 package com.hoonjin.study.spring.hello;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 @RestController
-public class HelloController {
+public class HelloController implements ApplicationContextAware {
 
     private final HelloService helloService;
+    private ApplicationContext applicationContext;
 
     public HelloController(HelloService helloService) {
         this.helloService = helloService;
@@ -17,5 +21,12 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
+    }
+
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("applicationContext = " + applicationContext);
+        this.applicationContext = applicationContext;
     }
 }
--- spring/spring-hello/src/main/java/com/hoonjin/study/spring/hello/SpringHelloApplication.java
+++ spring/spring-hello/src/main/java/com/hoonjin/study/spring/hello/SpringHelloApplication.java
@@ -1,34 +1,28 @@
 package com.hoonjin.study.spring.hello;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
 @ComponentScan
 public class SpringHelloApplication {
 
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
     public static void main(String[] args) {
-
-        AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-                WebServer webServer = serverFactory.getWebServer(servletContext ->
-                    servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this))
-                        .addMapping("/*")
-                );
-                webServer.start();
-            }
-        };
-
-        ac.register(SpringHelloApplication.class);
-        ac.refresh();
+        SpringApplication.run(SpringHelloApplication.class, args);
     }
 }
