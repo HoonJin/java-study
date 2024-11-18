31eb1f17b15c8312dce94082f8af4d0f93db1790
Daniel Ji <bwjhj1030@gmail.com>
2024-11-18 11:53:25 +0900

    actuator 예제 추가

---
+++ spring/spring-btcores/spring-btcores-actuator/src/main/java/com/hoonjin/study/spring/btcores/actuator/ActuatorApplication.java
@@ -0,0 +1,19 @@
+package com.hoonjin.study.spring.btcores.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+    @Bean
+    public InMemoryHttpExchangeRepository exchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
+    }
+}
