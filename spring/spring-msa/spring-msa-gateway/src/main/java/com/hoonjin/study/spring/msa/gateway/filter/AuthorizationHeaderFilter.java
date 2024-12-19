d8148fab6813f8b0e150b32487d6f972104482e3
Daniel Ji <bwjhj1030@gmail.com>
2024-12-19 13:31:02 +0900

    msa 중 config server와 gateway service 추가

---
+++ spring/spring-msa/spring-msa-gateway/src/main/java/com/hoonjin/study/spring/msa/gateway/filter/AuthorizationHeaderFilter.java
@@ -0,0 +1,92 @@
+package com.hoonjin.study.spring.msa.gateway.filter;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@Component
+@Slf4j
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    private final Environment environment;
+
+    public AuthorizationHeaderFilter(Environment environment) {
+        super(Config.class);
+        this.environment = environment;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            if (request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                String authorization = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+                String token = authorization.replace("Bearer ", "");
+
+                if (isValidJwt(token)) {
+                    return chain.filter(exchange);
+                } else {
+                    return onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
+                }
+            } else {
+                log.error("invalid jwt but pass");
+                return chain.filter(exchange);
+            }
+        };
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        return response.setComplete();
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        log.error(err);
+
+        byte[] bytes = "The requested token is invalid.".getBytes(StandardCharsets.UTF_8);
+        DataBuffer buffer = exchange.getResponse().bufferFactory().wrap(bytes);
+        return response.writeWith(Flux.just(buffer));
+    }
+
+    private boolean isValidJwt(String token) {
+        byte[] secretKeyBytes = Base64.getEncoder().encode(environment.getProperty("token.secret").getBytes());
+        SecretKey signingKey = new SecretKeySpec(secretKeyBytes, SignatureAlgorithm.HS512.getJcaName());
+
+        try {
+            String subject = Jwts.parserBuilder()
+                .setSigningKey(signingKey)
+                .build()
+                .parseClaimsJws(token).getBody()
+                .getSubject();
+            return !(subject == null || subject.isEmpty());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    @Data
+    public static class Config {
+
+    }
+}
