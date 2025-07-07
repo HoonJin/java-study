9e23bd624db44704566a94102c6c89e70a92a2a0
Daniel Ji <bwjhj1030@gmail.com>
2025-07-07 18:59:00 +0900

    메타 어노테이션을 이용한 합성 어노테이션을 적용하여 메인 클래스를 스프링 부트 기본과 유사하게 만듬

--- spring/spring-hello/src/main/java/com/hoonjin/study/spring/hello/SpringHelloApplication.java
+++ spring/spring-hello/src/main/java/com/hoonjin/study/spring/hello/SpringHelloApplication.java
@@ -1,28 +1,12 @@
 package com.hoonjin.study.spring.hello;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
-@ComponentScan
+@MySpringBootApplication
 public class SpringHelloApplication {
-
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringHelloApplication.class, args);
     }
+
 }
