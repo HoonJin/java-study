63125ecea1f1bece4f964fb8162e850c6301a003
Daniel Ji <bwjhj1030@gmail.com>
2025-07-08 16:54:26 +0900

    토비의 클린 스프링 - 도메인 모델 패턴과 헥사고날 아키텍처 Part 1 강의 프로젝트 생성과 문서 작성[

---
+++ spring/spring-splearn/src/main/java/com/hoonjin/study/spring/splearn/SplearnApplication.java
@@ -0,0 +1,12 @@
+package com.hoonjin.study.spring.splearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplearnApplication.class, args);
+    }
+}
