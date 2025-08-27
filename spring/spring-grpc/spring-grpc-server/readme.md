72aaec220ab0a5f2f6d8e63640ac55261590dfcf
Daniel Ji <bwjhj1030@gmail.com>
2025-08-27 16:05:48 +0900

    GRPC 서버 프로젝트를 멀티 모듈로 재구성하여 프로토콜버퍼를 향후 클라이언트와 공유할 수 있도록 구조 개선과 user.proto 버저닝 포함

--- spring/spring-grpc/spring-grpc-server/readme.md
+++ spring/spring-grpc/spring-grpc-server/readme.md
@@ -1,7 +1,7 @@
 ### 요청 예제 (intellij)
 ```shell
 ###
-GRPC localhost:9090/user.UserService/GetUser
+GRPC localhost:9090/user.v1.UserService/GetUser
 
 {
   "id": 1
--- spring/spring-grpc/spring-grpc-server/src/main/java/com/hoonjin/study/spring/grpc/server/UserGrpcService.java
+++ spring/spring-grpc/spring-grpc-server/src/main/java/com/hoonjin/study/spring/grpc/server/UserGrpcService.java
@@ -1,5 +1,6 @@
 package com.hoonjin.study.spring.grpc.server;
 
+import com.hoonjin.study.spring.grpc.proto.user.*;
 import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.grpc.server.service.GrpcService;
@@ -10,39 +11,37 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
-import static com.hoonjin.study.spring.grpc.server.UserProto.*;
-
 @GrpcService
 @Slf4j
 public class UserGrpcService extends UserServiceGrpc.UserServiceImplBase {
-    
+
     // 간단한 인메모리 저장소
     private final Map<Long, UserResponse> users = new HashMap<>();
     private final AtomicLong idGenerator = new AtomicLong(1);
-    
+
     public UserGrpcService() {
         // 초기 데이터 추가
         users.put(1L, UserResponse.newBuilder()
-                .setId(1)
-                .setName("홍길동")
-                .setEmail("hong@example.com")
-                .setCreatedAt(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
-                .build());
-        
+            .setId(1)
+            .setName("홍길동")
+            .setEmail("hong@example.com")
+            .setCreatedAt(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+            .build());
+
         users.put(2L, UserResponse.newBuilder()
-                .setId(2)
-                .setName("김영희")
-                .setEmail("kim@example.com")
-                .setCreatedAt(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
-                .build());
+            .setId(2)
+            .setName("김영희")
+            .setEmail("kim@example.com")
+            .setCreatedAt(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+            .build());
     }
-    
+
     @Override
     public void getUser(GetUserRequest request, StreamObserver<UserResponse> responseObserver) {
         log.info("GetUser 요청: ID {}", request.getId());
-        
+
         UserResponse user = users.get(request.getId());
-        
+
         if (user != null) {
             responseObserver.onNext(user);
         } else {
@@ -54,33 +53,33 @@ public class UserGrpcService extends UserServiceGrpc.UserServiceImplBase {
             );
             return;
         }
-        
+
         responseObserver.onCompleted();
     }
-    
+
     @Override
     public void createUser(CreateUserRequest request, StreamObserver<UserResponse> responseObserver) {
         log.info("CreateUser 요청: 이름 {}, 이메일 {}", request.getName(), request.getEmail());
-        
+
         long newId = idGenerator.getAndIncrement();
-        
+
         UserResponse newUser = UserResponse.newBuilder()
-                .setId(newId)
-                .setName(request.getName())
-                .setEmail(request.getEmail())
-                .setCreatedAt(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
-                .build();
-        
+            .setId(newId)
+            .setName(request.getName())
+            .setEmail(request.getEmail())
+            .setCreatedAt(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+            .build();
+
         users.put(newId, newUser);
-        
+
         responseObserver.onNext(newUser);
         responseObserver.onCompleted();
     }
-    
+
     @Override
     public void getUsersStream(GetUsersRequest request, StreamObserver<UserResponse> responseObserver) {
         log.info("GetUsersStream 요청: 페이지 {}, 사이즈 {}", request.getPage(), request.getSize());
-        
+
         // 스트리밍으로 사용자 목록 전송
         users.values().forEach(user -> {
             try {
@@ -93,7 +92,7 @@ public class UserGrpcService extends UserServiceGrpc.UserServiceImplBase {
                 responseObserver.onError(e);
             }
         });
-        
+
         responseObserver.onCompleted();
     }
 }
